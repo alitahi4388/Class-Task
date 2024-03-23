cf4b5894429214c33e895d944ac33469c1f316b8
alitahi4388 <163639372+alitahi4388@users.noreply.github.com>
2024-03-23 17:17:30 +0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+function update(){
+    console.log("updating results");
+
+var input = document.getElementById('myinput');
+var output = document.getElementById('output');
+output.innerHTML = input.Value;
+
+}
